e4b9fd6b6030b642aeb776b5a1949a545f97c8ef
Easton Morris <eastonmorris9@gmail.com>
2021-11-29 20:13:39 -0800

    fleshed out more of the header component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,7 +5,28 @@ export default class Header extends React.Component {
     super(props);
 
     this.state = {
+      loggedIn: null,
       pages: []
     };
+  }
+
+  render() {
+    return (
+      <header className="mb-5">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <a href="#" className="navbar-brand">
+              <img src="../favicon.ico" alt="Chase.me Icon" />Chase.me
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-colapse" id="navbarToggler">
+
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
   }
 }
