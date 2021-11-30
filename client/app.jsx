4d7795bd66e57d21c2d986955521a1ffd4c8097c
Easton Morris <eastonmorris9@gmail.com>
2021-11-29 21:14:39 -0800

    added an index.js for routing the library methods and completed the header with an update in the app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import Home from './pages/home';
+import Header from './components/header';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState(prevState => (
+        { route: parseRoute(window.location.hash) }
+      ));
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '') {
+      return <Home />;
+    }
+  }
+
   render() {
-    return <Home />;
+    return (
+    <>
+      <Header />
+      { this.renderPage() }
+    </>
+    );
   }
 }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
 
     this.state = {
       loggedIn: null,
-      pages: []
+      lists: []
     };
   }
 
@@ -21,8 +21,18 @@ export default class Header extends React.Component {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-colapse" id="navbarToggler">
-
+            <div className="collapse navbar-collapse" id="navbarToggler">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item dropdown">
+                  <a href="#mylists" className="nav-link dropdown-toggle" id="listsDropdown">My Lists</a>
+                  <ul className="dropdown-menu" aria-labelledby="listsDropdown">
+                    <li><a href="#mylists">1</a></li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a href="#info" className="nav-link">Info</a>
+                </li>
+              </ul>
             </div>
           </div>
         </nav>
