919e63a64c6777f1e7ac59e7a7ccb7a4764dfcef
Easton Morris <eastonmorris9@gmail.com>
2021-11-30 20:07:56 -0800

    created the list page file

---
+++ client/pages/list.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class lists extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: null
+    };
+  }
+
+  componentDidMount() {
+
+  }
+}
