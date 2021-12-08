4d8ed5e9a3655843461d110d7cadfac348f3dc5a
Easton Morris <eastonmorris9@gmail.com>
2021-12-07 21:16:50 -0800

    added the modal search functionality that searches for cards onSubmit

--- client/components/search-modal.jsx
+++ client/components/search-modal.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import pokemon from 'pokemontcgsdk';
+
+pokemon.configure({ apiKey: '311567c3-099d-4c46-b373-5189bd577b08' });
 
 export default class SearchModal extends React.Component {
   constructor(props) {
@@ -8,250 +11,12 @@ export default class SearchModal extends React.Component {
     this.SearchModalItem = this.SearchModalItem.bind(this);
     this.SearchResultsBody = this.SearchResultsBody.bind(this);
     this.DatalistItem = this.DatalistItem.bind(this);
+    this.searchHandler = this.searchHandler.bind(this);
 
     this.state = {
-      pokeList: [
-        {
-          id: 'swsh4-26',
-          name: 'Charizards',
-          supertype: 'Pokémon',
-          subtypes: [
-            'Stage 2'
-          ],
-          hp: '170',
-          types: [
-            'Fire'
-          ],
-          evolvesFrom: 'Charmeleon',
-          abilities: [
-            {
-              name: 'Battle Sense',
-              text: 'Once during your turn, you may look at the top 3 cards of your deck and put 1 of them into your hand. Discard the other cards.',
-              type: 'Ability'
-            }
-          ],
-          attacks: [
-            {
-              name: 'Royal Blaze',
-              cost: [
-                'Fire',
-                'Fire'
-              ],
-              convertedEnergyCost: 2,
-              damage: '100+',
-              text: 'This attack does 50 more damage for each Leon card in your discard pile.'
-            }
-          ],
-          weaknesses: [
-            {
-              type: 'Water',
-              value: '×2'
-            }
-          ],
-          retreatCost: [
-            'Colorless',
-            'Colorless',
-            'Colorless'
-          ],
-          convertedRetreatCost: 3,
-          set: {
-            id: 'swsh4',
-            name: 'Vivid Voltage',
-            series: 'Sword & Shield',
-            printedTotal: 185,
-            total: 203,
-            legalities: {
-              unlimited: 'Legal',
-              standard: 'Legal',
-              expanded: 'Legal'
-            },
-            ptcgoCode: 'VIV',
-            releaseDate: '2020/11/13',
-            updatedAt: '2020/11/13 16:20:00',
-            images: {
-              symbol: 'https://images.pokemontcg.io/swsh4/symbol.png',
-              logo: 'https://images.pokemontcg.io/swsh4/logo.png'
-            }
-          },
-          number: '25',
-          artist: 'Ryuta Fuse',
-          rarity: 'Rare',
-          flavorText: 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
-          nationalPokedexNumbers: [
-            6
-          ],
-          legalities: {
-            unlimited: 'Legal',
-            standard: 'Legal',
-            expanded: 'Legal'
-          },
-          images: {
-            small: 'https://images.pokemontcg.io/swsh4/25.png',
-            large: 'https://images.pokemontcg.io/swsh4/25_hires.png'
-          },
-          tcgplayer: {
-            url: 'https://prices.pokemontcg.io/tcgplayer/swsh4-25',
-            updatedAt: '2021/08/04',
-            prices: {
-              normal: {
-                low: 1.73,
-                mid: 3.54,
-                high: 12.99,
-                market: 2.82,
-                directLow: 3.93
-              },
-              reverseHolofoil: {
-                low: 3,
-                mid: 8.99,
-                high: 100,
-                market: 3.89,
-                directLow: 4.46
-              }
-            }
-          },
-          cardmarket: {
-            url: 'https://prices.pokemontcg.io/cardmarket/swsh4-25',
-            updatedAt: '2021/08/04',
-            prices: {
-              averageSellPrice: 9.38,
-              lowPrice: 8.95,
-              trendPrice: 10.29,
-              germanProLow: null,
-              suggestedPrice: null,
-              reverseHoloSell: null,
-              reverseHoloLow: null,
-              reverseHoloTrend: null,
-              lowPriceExPlus: 8.95,
-              avg1: 9.95,
-              avg7: 9.35,
-              avg30: 11.31,
-              reverseHoloAvg1: null,
-              reverseHoloAvg7: null,
-              reverseHoloAvg30: null
-            }
-          }
-        },
-        {
-          id: 'swsh4-25',
-          name: 'Charizard',
-          supertype: 'Pokémon',
-          subtypes: [
-            'Stage 2'
-          ],
-          hp: '170',
-          types: [
-            'Fire'
-          ],
-          evolvesFrom: 'Charmeleon',
-          abilities: [
-            {
-              name: 'Battle Sense',
-              text: 'Once during your turn, you may look at the top 3 cards of your deck and put 1 of them into your hand. Discard the other cards.',
-              type: 'Ability'
-            }
-          ],
-          attacks: [
-            {
-              name: 'Royal Blaze',
-              cost: [
-                'Fire',
-                'Fire'
-              ],
-              convertedEnergyCost: 2,
-              damage: '100+',
-              text: 'This attack does 50 more damage for each Leon card in your discard pile.'
-            }
-          ],
-          weaknesses: [
-            {
-              type: 'Water',
-              value: '×2'
-            }
-          ],
-          retreatCost: [
-            'Colorless',
-            'Colorless',
-            'Colorless'
-          ],
-          convertedRetreatCost: 3,
-          set: {
-            id: 'swsh4',
-            name: 'Vivid Voltage',
-            series: 'Sword & Shield',
-            printedTotal: 185,
-            total: 203,
-            legalities: {
-              unlimited: 'Legal',
-              standard: 'Legal',
-              expanded: 'Legal'
-            },
-            ptcgoCode: 'VIV',
-            releaseDate: '2020/11/13',
-            updatedAt: '2020/11/13 16:20:00',
-            images: {
-              symbol: 'https://images.pokemontcg.io/swsh4/symbol.png',
-              logo: 'https://images.pokemontcg.io/swsh4/logo.png'
-            }
-          },
-          number: '25',
-          artist: 'Ryuta Fuse',
-          rarity: 'Rare',
-          flavorText: 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
-          nationalPokedexNumbers: [
-            6
-          ],
-          legalities: {
-            unlimited: 'Legal',
-            standard: 'Legal',
-            expanded: 'Legal'
-          },
-          images: {
-            small: 'https://images.pokemontcg.io/swsh4/25.png',
-            large: 'https://images.pokemontcg.io/swsh4/25_hires.png'
-          },
-          tcgplayer: {
-            url: 'https://prices.pokemontcg.io/tcgplayer/swsh4-25',
-            updatedAt: '2021/08/04',
-            prices: {
-              normal: {
-                low: 1.73,
-                mid: 3.54,
-                high: 12.99,
-                market: 2.82,
-                directLow: 3.93
-              },
-              reverseHolofoil: {
-                low: 3,
-                mid: 8.99,
-                high: 100,
-                market: 3.89,
-                directLow: 4.46
-              }
-            }
-          },
-          cardmarket: {
-            url: 'https://prices.pokemontcg.io/cardmarket/swsh4-25',
-            updatedAt: '2021/08/04',
-            prices: {
-              averageSellPrice: 9.38,
-              lowPrice: 8.95,
-              trendPrice: 10.29,
-              germanProLow: null,
-              suggestedPrice: null,
-              reverseHoloSell: null,
-              reverseHoloLow: null,
-              reverseHoloTrend: null,
-              lowPriceExPlus: 8.95,
-              avg1: 9.95,
-              avg7: 9.35,
-              avg30: 11.31,
-              reverseHoloAvg1: null,
-              reverseHoloAvg7: null,
-              reverseHoloAvg30: null
-            }
-          }
-        }
-      ]
+      pokeList: [],
+      nameList: [],
+      searchValue: 'Jolteon'
     };
 
   }
@@ -298,11 +63,27 @@ export default class SearchModal extends React.Component {
 
   }
 
-  onSearchChangeHandler() {
-
+  searchHandler(event) {
+    const $pokeSearch = document.getElementById('cardSearch');
+    if ($pokeSearch.value !== this.state.searchValue) {
+      pokemon.card.all({ q: `name:${$pokeSearch.value}*` })
+        .then(result => {
+          this.setState({
+            pokeList: result,
+            searchValue: $pokeSearch.value
+          });
+        });
+    }
+    event.preventDefault();
   }
 
   componentDidMount() {
+    pokemon.card.all({ q: 'name:jolteon' })
+      .then(result => {
+        this.setState({
+          pokeList: result
+        });
+      });
 
   }
 
@@ -321,9 +102,18 @@ export default class SearchModal extends React.Component {
                 <div className="search-text">
                   <h4>Search by Name</h4>
                   <div className="search-area">
-                    <label htmlFor="cardSearch" className="form-label">Card Search</label>
-                    <input className="form-control" list="pokeList" id="cardSearch" placeholder="Jolteon"/>
-                    <this.SearchDatalist />
+                    <form action="" onSubmit={this.searchHandler}>
+                      <label htmlFor="cardSearch" className="form-label">Card Search</label>
+                      <div className="row">
+                        <div className="col-auto">
+                          <input className="form-control" list="pokeList" id="cardSearch" placeholder="Jolteon"/>
+                          <this.SearchDatalist />
+                        </div>
+                        <div className="col-auto">
+                          <button type="submit" id="pokeSearchBtn" className="btn btn-primary">Search</button>
+                        </div>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
