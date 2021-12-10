ea7e27845c82a727f60ed272713253459dd31e35
Easton Morris <eastonmorris9@gmail.com>
2021-12-09 22:43:31 -0800

    imported the schema and set up the db, wrote the card update script, now in testing

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,49 @@
 set client_min_messages to warning;
-
 -- DANGER: this is NOT how to do it in the real world.
 -- `drop schema` INSTANTLY ERASES EVERYTHING.
 drop schema "public" cascade;
-
 create schema "public";
+CREATE TABLE "users" (
+    "userId" serial NOT NULL,
+    "username" TEXT NOT NULL UNIQUE,
+    "email" TEXT NOT NULL UNIQUE,
+    "hashPw" TEXT NOT NULL,
+    "createdAt" timestamptz NOT NULL DEFAULT now(),
+    CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "lists" (
+    "userId" integer NOT NULL,
+    "listId" serial NOT NULL,
+    "listName" TEXT NOT NULL,
+    "cards" TEXT NOT NULL,
+    "capped" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "lists_pk" PRIMARY KEY ("listId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "cards" (
+    "cardId" TEXT NOT NULL,
+    "cardName" TEXT NOT NULL,
+    "setName" TEXT NOT NULL,
+    "smallPic" TEXT NOT NULL UNIQUE,
+    "largePic" TEXT NOT NULL UNIQUE,
+    "cardNum" TEXT NOT NULL,
+    "cardObj" TEXT NOT NULL,
+    "cardObjDate" timestamptz NOT NULL DEFAULT now(),
+    CONSTRAINT "cards_pk" PRIMARY KEY ("cardId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "cardLists" (
+    "listId" integer NOT NULL,
+    "cardId" TEXT NOT NULL,
+    "createdAt" timestamptz NOT NULL DEFAULT now(),
+    CONSTRAINT "cardLists_pk" PRIMARY KEY ("listId", "cardId")
+) WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "lists" ADD CONSTRAINT "lists_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "cardLists" ADD CONSTRAINT "cardLists_fk0" FOREIGN KEY ("listId") REFERENCES "lists"("listId");
+ALTER TABLE "cardLists" ADD CONSTRAINT "cardLists_fk1" FOREIGN KEY ("cardId") REFERENCES "cards"("cardId");
---
+++ database/update-cards.js
@@ -0,0 +1,56 @@
+const pokemon = require('pokemontcgsdk');
+pokemon.configure({ apiKey: '311567c3-099d-4c46-b373-5189bd577b08' });
+
+require('dotenv/config');
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const checkSql = `
+  SELECT *
+    FROM "cards"
+    WHERE "cardId" = $1
+`;
+
+// const insertSql = `
+//   INSERT INTO "cards" ("cardId", "cardName", "setName", "smallPic", "largePic", "cardNum", "cardObj")
+//   VALUES ($1, $2, $3, $4, $5, $6, $7)
+// `;
+
+for (let ii = 1; ii < 100; ii++) {
+  pokemon.card.where({ page: ii })
+    .then(result => {
+      // console.log(result);
+      if (result.count === 0) {
+        // eslint-disable-next-line
+        console.log('reached end of list.');
+        return ii;
+      } else {
+        for (let jj = 0; jj < 250; jj++) {
+          const currentCard = result.data[jj];
+          // console.log(currentCard);
+          // console.log(jj);
+          const params = [currentCard.id];
+          db.query(checkSql, params)
+            .then(result => {
+              // console.log(result);
+              if (result.rowCount === 0) {
+                // eslint-disable-next-line
+                // console.log(currentCard);
+              // const params = [currentCard.id, currentCard.name, currentCard.set.name, currentCard.images.small, currentCard.images.large, currentCard.number, currentCard];
+              //   db.query(insertSql, params);
+              }
+            })
+            // eslint-disable-next-line
+            .catch(Error => console.log(Error));
+        }
+      }
+    })
+    // eslint-disable-next-line
+    .catch(Error => console.log(Error));
+}
