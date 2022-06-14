bfe82d856620bee449da8e8820eb38ce0f777443
Easton Morris <eastonmorris9@gmail.com>
2022-06-14 10:39:22 -0700

    fixed syntax on values in data.sql

--- database/data.sql
+++ database/data.sql
@@ -2,8 +2,8 @@ INSERT INTO "users" ("username", "email", "hashPw")
 VALUES ('admin', 'admin@test.com', '$argon2i$v=19$m=4096,t=3,p=1$gRptO+CxNhYRr3SkbyfpJQ$COtXF9uxxIEiHqDW+v0Q8fmPUD3Gmj/+jlp6kkwMPpQ')
 RETURNING *;
 
-INSERT INTO "lists" ("userId", "listName", "capped")
-VALUES (1, 'ExampleList', false)
+INSERT INTO "lists" ("userId", "listName", "cards", "capped")
+VALUES (1, 'ExampleList', '{}', false)
 RETURNING *;
 
 INSERT INTO "cardLists" ("listId", "cardId")
